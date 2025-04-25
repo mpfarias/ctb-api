4ee88e543dc348f2ed06db0ca3933e629f2d878c
Marcelo Farias <mpfarias85@gmail.com>
2025-04-24 23:56:46 -0300

    API finalizada

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ import express from 'express'
 import infracaoRoutes from './routes/infracao.routes.js'
 import swaggerUi from 'swagger-ui-express'
 import { swaggerSpec } from './docs/swagger.js'
+import rateLimit from 'express-rate-limit'
+import morgan from 'morgan'
 
 const app = express()
 const PORT =  process.env.PORT || 3000
 
+app.use(morgan('dev'))
+
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, 
+  max: 50,
+  message: 'Muitas requisições. Tente novamente em 2 minutos.',
+  standardHeaders: true, // Exibe os headers de rate limit no response
+  legacyHeaders: false // Desativa headers antigos (X-RateLimit-*)
+})
+
+app.use(limiter) // Aplica globalmente o rate limit
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -16,5 +29,5 @@ app.use('/infracoes', infracaoRoutes)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.listen(PORT, () => {
-  console.log(`🚦 Servidor rodando em http://localhost:${PORT}`)
+  console.log(`Servidor rodando em http://localhost:${PORT}`)
 })
--- controllers/infracao.controller.js
+++ controllers/infracao.controller.js
@@ -1,26 +1,50 @@
 import fs from 'fs/promises'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import NodeCache from 'node-cache'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const dataPath = path.join(__dirname, '../data/infracoes.json')
 
+const cache = new NodeCache({ stdTTL: 300 })
+
 export const getInfracoes = async (req, res) => {
-  const data = await fs.readFile(dataPath, 'utf-8')
-  const infracoes = JSON.parse(data)
-  res.json(infracoes)
+  const cached = cache.get('infracoes')
+  if (cached) {
+    console.log('✅ Resposta enviada do cache')
+    return res.json(cached)
+  }
+
+  try {
+    const data = await fs.readFile(dataPath, 'utf-8')
+    const infracoes = JSON.parse(data)
+
+    cache.set('infracoes', infracoes)
+    console.log('📦 Dados salvos no cache')
+
+    res.json(infracoes)
+  } catch (err) {
+    console.error('Erro ao carregar infrações:', err)
+    res.status(500).json({ erro: 'Erro ao carregar as infrações' })
+  }
 }
 
 export const getInfracaoPorArtigo = async (req, res) => {
   const { artigo } = req.params
-  const data = await fs.readFile(dataPath, 'utf-8')
-  const infracoes = JSON.parse(data)
-  const infracao = infracoes.find(i => String(i.artigo) === String(artigo))
 
-  if (!infracao) {
-    return res.status(404).json({ mensagem: 'Infração não encontrada' })
+  try {
+    const data = await fs.readFile(dataPath, 'utf-8')
+    const infracoes = JSON.parse(data)
+    const infracao = infracoes.find(i => String(i.artigo) === String(artigo))
+
+    if (!infracao) {
+      return res.status(404).json({ mensagem: 'Infração não encontrada' })
+    }
+
+    res.json(infracao)
+  } catch (err) {
+    console.error('Erro ao buscar artigo:', err)
+    res.status(500).json({ erro: 'Erro ao buscar artigo' })
   }
-
-  res.json(infracao)
 }
