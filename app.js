4a17b28018797a4ac0214f3bc7f77788b7265c5c
Marcelo Farias <mpfarias85@gmail.com>
2025-04-25 00:24:26 -0300

    API concluída

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import morgan from 'morgan'
 const app = express()
 const PORT =  process.env.PORT || 3000
 
-app.use(morgan('dev'))
+
 
 const limiter = rateLimit({
   windowMs: 2 * 60 * 1000, 
@@ -20,6 +20,7 @@ const limiter = rateLimit({
 
 app.use(limiter)
 app.use(express.json())
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   res.send('Infrações CTB!\nGET /infracoes - Lista todas as infrações disponíveis no CTB.\nGET /infracoes/:artigo - Retorna os dados da infração com base no número do artigo.\nGET /infracoes/codigo/:codigo - Retorna a infração com base no código RENAINF.')
@@ -28,6 +29,4 @@ app.get('/', (req, res) => {
 app.use('/infracoes', infracaoRoutes)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`)
-})
+export default app
